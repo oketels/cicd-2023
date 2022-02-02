3ad627ab5e09cb7e8e97db1b91a3bc6638d26fe2
Olivier Ketels <olivier.ketels@gmail.com>
2022-02-02 14:57:09 +0100

    Genesis.

---
+++ src/test/java/be/xplore/cicd2022/Cicd2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.xplore.cicd2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2022ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
